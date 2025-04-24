a2912d1c781ab20da8aab8b03cdc54e623c0083d
thingadoo <flinge1338@gmail.com>
2025-04-24 19:10:36 +0700

    Did stuff

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -9,21 +9,21 @@ function changeFont() {
   if (decider.checked || fontbox.includes("fontboxchecked=1")) {
     li.forEach(element => {
       element.style.fontFamily = "Open Dyslexic";
-      element.style.fontSize = "1.1em";
+      element.style.fontSize = "1.3em";
     });
     p.forEach(element => {
       element.style.fontFamily = "Open Dyslexic";
-      element.style.fontSize = "1.1em";
+      element.style.fontSize = "1.3em";
     });
     document.cookie = "fontboxchecked=1; path=/";
   } else if (!decider.checked) {
     li.forEach(element => {
       element.style.fontFamily = "Warung Kopi";
-      element.style.fontSize = "1.3em";
+      element.style.fontSize = "1.6em";
     });
     p.forEach(element => {
       element.style.fontFamily = "Warung Kopi";
-      element.style.fontSize = "1.3em";
+      element.style.fontSize = "1.6em";
     });
   }
 }
